7f2ed4f9c588727446ed9724c16cef15c6208989
Reema Dhanwani <reemadhanwani1234@gmail.com>
2024-12-11 19:41:24 +0530

    Added basic layout with sidebar and main data partition

--- src/Components/MainData/MainData.tsx
+++ src/Components/MainData/MainData.tsx
@@ -1,8 +1,8 @@
 function MainData() {
     return(
-        <>
+        <div className="basis-9/12 text-black h-full bg-[#f6f6f8] rounded-tr-3xl rounded-br-3xl">
             MainData
-        </>
+        </div>
     )
 }
 
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,15 +1,16 @@
 import MainData from "../Components/MainData/MainData";
 import Sidebar from "../Components/Sidebar/Sidebar";
+import img from '../Assets/night.jpg';
 
 function Home() {
     return(
-        <>
-            <div>
+        <div className="h-[100vh]" style={{backgroundImage: `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)),url(${img})`}}>
+            <div className="h-full mx-8 py-8 flex flex-row align-center items-center">
                 <Sidebar/>
                 <MainData/>
             </div>
            
-        </>
+        </div>
     );
 }
 
