ad54c56633e6a5d90612914d6eeec452056450b4
Reema Dhanwani <reemadhanwani1234@gmail.com>
2024-12-13 16:07:47 +0530

    Added expandable rows to maindata

--- src/Components/MainData/WeekRow.tsx
+++ src/Components/MainData/WeekRow.tsx
@@ -1,8 +1,17 @@
+import TempCard from "../TempCard";
+import CloudyTemp from '../../Assets/Cloudytemp.png';
+
+
 function WeekRow() {
     return (
-        <div>
-            WeekRow
-        </div>
+        <div className="w-full px-12 py-2 flex flex-wrap gap-5">
+        <TempCard title="Friday" temperature="25.3°C" image={CloudyTemp} />
+        <TempCard title="Friday" temperature="25.3°C" image={CloudyTemp} />
+        <TempCard title="Friday" temperature="25.3°C" image={CloudyTemp} />
+        <TempCard title="Friday" temperature="25.3°C" image={CloudyTemp} />
+        <TempCard title="Friday" temperature="25.3°C" image={CloudyTemp} />
+        <TempCard title="Friday" temperature="25.3°C" image={CloudyTemp} />
+    </div>
     )
 }
 export default WeekRow;
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -4,12 +4,9 @@ import img from '../Assets/night.jpg';
 
 function Home() {
     return(
-        <div className="h-[100vh]" style={{backgroundSize: 'cover' ,backgroundImage: `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)),url(${img})`}}>
-            <div className="h-full mx-8 py-8 flex flex-row align-center items-center">
+        <div className="min-h-[100vh] px-8 py-8 flex flex-row justify-center items-stretch" style={{backgroundSize: 'cover', backgroundImage: ` linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${img})`}}>
                 <Sidebar/>
                 <MainData/>
-            </div>
-           
         </div>
     );
 }
