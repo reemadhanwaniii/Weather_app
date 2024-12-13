8dea8caf4123f93d83897434fd18f8c225482dbb
Reema Dhanwani <reemadhanwani1234@gmail.com>
2024-12-13 21:37:25 +0530

    Added redux and required interfaces

---
+++ src/Interfaces/ForecastData.ts
@@ -0,0 +1,7 @@
+import CurrentDayForecast from "./CurrentDayForecast";
+import DayForecast from "./DayForecast";
+export default interface ForecastData {
+    location: {localtime: string, country: string, region: string, name: string},
+    dayForecast: DayForecast[],
+    currentData: CurrentDayForecast
+}
